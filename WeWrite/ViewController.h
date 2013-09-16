266268556582cb10614953a893e1abb794989051
shilinfeng <shilinf@umich.edu>
2013-09-16 19:41:31 -0400

    created view

---
+++ WeWrite/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WeWrite
+//
+//  Created by Linfeng Shi on 9/16/13.
+//  Copyright (c) 2013 Linfeng Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
